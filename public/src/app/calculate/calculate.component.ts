45d6dc390bcbb5ff929d8201a82046578c8a6f9a
Aria Samandi <ariasamandi@gmail.com>
2018-10-14 21:49:17 -0700

    commit

--- public/src/app/calculate/calculate.component.ts
+++ public/src/app/calculate/calculate.component.ts
@@ -10,20 +10,52 @@ export class CalculateComponent implements OnInit {
   constructor(private _http: HttpService) { }
   Breakfast: any;
   calories: any;
+  protein: any;
+  carbs: any;
+  fats: any;
 
   ngOnInit() {
     this.sum();
   }
   sum(){
     this.calories = 0;
+    this.protein = 0;
+    this.carbs = 0;
+    this.fats = 0;
     this._http.allBreakfast().subscribe(all=>{
       console.log(all[0].calories);
       console.log("the length is", all.length)
       for(var i = 0; i < all.length; i++){
         this.calories += all[i].calories;
+        this.protein += all[i].protein;
+        this.carbs += all[i].carbs;
+        this.fats += all[i].fat;
+
       }
-      console.log(this.calories)
     })
+      this._http.allLunch().subscribe(all=>{
+        console.log(all[0].calories);
+        console.log("the length is", all.length)
+        for(var i = 0; i < all.length; i++){
+          this.calories += all[i].calories;
+          this.protein += all[i].protein;
+          this.carbs += all[i].carbs;
+          this.fats += all[i].fat;
+  
+        }
+      })
+        this._http.allDinner().subscribe(all=>{
+          console.log(all[0].calories);
+          console.log("the length is", all.length)
+          for(var i = 0; i < all.length; i++){
+            this.calories += all[i].calories;
+            this.protein += all[i].protein;
+            this.carbs += all[i].carbs;
+            this.fats += all[i].fat;
+    
+          }
+        })
+      console.log(this.calories)
   }
   getBreakfast(){
     this._http.allBreakfast().subscribe(all=>{
